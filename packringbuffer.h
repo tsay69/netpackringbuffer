7a0a6660b3e697418b2d7824eb73cd1ceaa0a9b0
tsay69 <tsay69@163.com>
2014-04-01 19:25:03 +0800

    Create packringbuffer.h

---
+++ packringbuffer.h
@@ -0,0 +1,41 @@
+/*@@@@@@@ @@@@@@@
+    @@   @@
+   @@   @@@@@@@ 
+  @@        @@ 
+ @@   @@@@@@@*/
+
+#ifndef __TSH_netpackringbuffer__
+#define __TSH_netpackringbuffer__
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#define tPackLenType   int   //char short int
+
+struct netpackringbuffer;
+struct netpack;
+
+typedef struct netpackringbuffer   tNetPackRingBuffer;
+typedef struct netpack   tNetPack;
+
+
+
+
+struct netpackringbuffer* netpack_new(int size);
+
+int netpack_can_write(struct netpackringbuffer* rb);
+
+int netpack_can_read(struct netpackringbuffer* rb);
+
+int netpack_read(struct netpackringbuffer* rb, char* buffer);
+
+int netpack_write(struct netpackringbuffer* rb, char* buffer, int len);
+
+int netpack_recv(struct netpackringbuffer* rb, int fd);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
